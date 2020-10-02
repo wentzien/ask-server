ca90bc4b2085606e03f9e811193ca15e0d1fcbdc
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2020-10-02 10:50:22 +0200

    rewrite to WebSockets

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const cors = require("cors");
 const app = express();
-const questionRouter = require('./routes/questions');
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+const questionRouter = require("./routes/questions");
+const db = require("./db/mysql");
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use('/questions', questionRouter);
+io.on("connection", (socket) => {
+  console.log("new connection");
+
+  socket.on('join', async (id, callback) => {
+
+    const data = await db.all(id.id);
+
+    callback(data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user left");
+  });
+});
+
+app.use("/questions", questionRouter);
 
 // Port
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App listening on port ${port}...)`));
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`App listening on port ${port}...)`));
