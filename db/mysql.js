4e2c9948a860248a736c41dcd41f7bf7a3c219f9
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-07-30 18:51:41 +0200

    db envs

--- db/mysql.js
+++ db/mysql.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: '91.204.46.98',
-    port: 3306,
-    database: 'k131325_questionvotes',
-    user: 'k131325_question',
-    password: 's_65vuO1',
+    host: process.env.DB_HOST,
+    port: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 });
 
 const db = {};
