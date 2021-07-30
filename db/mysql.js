563ae799b41770b1960ada82ef594486436b2fd8
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-07-30 20:25:41 +0200

    fix port bug

--- db/mysql.js
+++ db/mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
-    port: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
